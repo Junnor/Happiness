b8d961784a08f18a938568cb53d69083c688bd0c
Junnor <dengquanzhu@gmail.com>
2016-03-09 15:44:35 +0800

    Lecture 5 Demo
    Smile

---
+++ Happiness/FaceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FaceViewController.swift
+//  Happiness
+//
+//  Created by Junor on 16/3/9.
+//  Copyright © 2016年 Junor. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+}
